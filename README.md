01051ed176b9db2eb011e567fe58380975e9982b
Laurent Meirlaen <burner@live.be>
2017-04-14 20:26:36 +0200

    add packing functionality + command line arguments + update readme

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# corona-archiver
-Python script to help pack and unpack Corona archive .car file
+# Corona Archiver
+
+Python script to help **pack** and **unpack** Corona archive .car file
+
+## Usage
+
+#### Unpacking
+``` Usage: corona-archiver.py -u 'input_file' 'output_dir' ```
+
+#### Packing
+``` Usage: corona-archiver.py -p 'input_dir' 'output_file' ```
+
+## File structure
+
+```
+[header]
+    [magic_number] (4 bytes) \x72\x61\x63\x01
+    [revision] (4 bytes)
+    [data_offset_start] (4 bytes)
+    [index_size] (4 bytes)
+
+[index]
+    [entry]
+        [entry_type] (4 bytes) 1
+        [data_offset] (4 bytes)
+        [filename_length] (4 bytes)
+        [filename] (filename_length+1 bytes) 0-terminated
+        [padding] (1|2|3|4 bytes) \x00
+
+[data]
+    [entry]
+        [entry_type] (4 bytes) 2
+        [next_data_offset] (4 bytes)
+        [file_size] (4 bytes)
+        [file_content] (file_size bytes)
+        [padding] (0|1|2|3 bytes) \x00
+
+[end]
+    [magic_number] (4 bytes) \xFF\xFF\xFF\xFF
+    [padding] (4 bytes) \x00
+```
+
+## Others languages
+
+Java : https://github.com/zhuowei/Chromosphere/tree/master/carlib
--- corona-archiver.py
+++ corona-archiver.py
@@ -1,13 +1,13 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
-import argparse
+import logging
 import os
 import pprint
 import struct
-import logging
+import sys
 
 
-class CoronaArchive:
+class CoronaArchiver:
     _MAGIC_NUMBER_HEADER = b'\x72\x61\x63\x01'  # ASCII = rac.
     _MAGIC_NUMBER_INDEX = 1
     _MAGIC_NUMBER_DATA = 2
@@ -24,9 +24,60 @@ class CoronaArchive:
     def __init__(self):
         pass
 
+    def __repr__(self):
+        return pprint.pformat(vars(self))
+
     def pack(self, input_dir, output_file):
         self.__input_dir = input_dir
-        logging.exception(NotImplementedError)
+
+        with open(output_file, 'wb+') as f:
+            self.stream = f
+
+            files = [name for name in os.listdir(self.__input_dir) if os.path.isfile(self.__input_dir + name)]
+            self.metadata['length'] = len(files)
+
+            # Write metadata
+            self.stream.write(self._MAGIC_NUMBER_HEADER)
+            self.stream.write(struct.pack('i', 1))  # revision
+            self.stream.write(struct.pack('i', 0))  # data_offset_start: temp writing as file content is not defined yet
+            self.stream.write(struct.pack('i', self.metadata['length']))  # length: number of index entries
+
+            # Write index entries
+            for filename in files:
+                padding_length = self._padding_length(len(filename), 'index')
+                offset = 1337  # temporary writing as data offset is not known yet
+                self.stream.write(struct.pack('iii', self._MAGIC_NUMBER_INDEX, offset, len(filename)))
+                self.stream.write(filename)
+                self._write_padding(padding_length)
+
+            # Update data_offset_start
+            tell = self.stream.tell()
+            data_offset_start = self.stream.tell() - 12  # tell() - tell() after writing data_offset_start value
+            self.stream.seek(8, 0)
+            self.stream.write(struct.pack('i', data_offset_start))
+            self.stream.seek(tell, 0)
+
+            # Write data entries
+            for filename in files:
+                with open(self.__input_dir + filename, 'rb') as f_append:
+                    length = os.path.getsize(self.__input_dir + filename)
+                    padding_length = self._padding_length(length, 'data')
+                    nxt = length + 4 + padding_length
+
+                    self.index[filename] = int(self.stream.tell())
+
+                    self.stream.write(struct.pack('iii', self._MAGIC_NUMBER_DATA, nxt, length))  # Write data header
+                    self.stream.write(f_append.read())  # Write file content
+                    self._write_padding(padding_length)  # Write padding
+
+            # Write end
+            self.stream.write(self._MAGIC_NUMBER_END)
+            self.stream.write(struct.pack('i', 0))
+
+            # Replace temporary "data_offset" values with final ones
+            self._write_finalize(files)
+
+        print "File {} successfully created.".format(output_file)
 
     def unpack(self, input_file, output_dir):
         self.__output_dir = output_dir
@@ -44,7 +95,7 @@ class CoronaArchive:
                 can_read = (i != self.metadata['length'] - 1)
                 self._read_index_entry(can_read)
 
-            # Jump to next section
+            # Jump to next section (jump over padding)
             while struct.unpack('b', f.read(1))[0] != self._MAGIC_NUMBER_DATA:
                 pass
             f.seek(-1, 1)
@@ -53,6 +104,45 @@ class CoronaArchive:
             self._read_data_idx()
 
             print "Extraction done."
+
+    @staticmethod
+    def _padding_length(length, type):
+        """ 
+            Padding to fill multiple of 4 length
+            Padding for index entries and data entries are different :
+                - index: if length is multiple of 4, no padding
+                - data: if length is multiple of 4, add 4*0x00 padding
+        """
+        padding = (length + (4 - length % 4)) - length
+        if type == 'data':
+            padding = padding if padding < 4 else 0
+
+        return padding
+
+    # PACKING
+
+    def _write_finalize(self, files):
+        self.stream.seek(16, 0)
+
+        self.stream.flush()
+
+        for filename in files:
+            padding_length = self._padding_length(len(filename), 'index')
+
+            dtype, = struct.unpack('i', self.stream.read(4))
+
+            self.stream.seek(self.stream.tell())  # fix Windows bug: http://bugs.python.org/issue1521491
+            self.stream.write(struct.pack('i', self.index[filename]))  # Write new offset value
+            self.stream.seek(self.stream.tell())  # fix Windows bug: http://bugs.python.org/issue1521491
+
+            length, = struct.unpack('i', self.stream.read(4))
+            self.stream.read(length + padding_length)
+
+    def _write_padding(self, padding):
+        for i in range(padding):
+            self.stream.write(b'\x00')
+
+    # UNPACKING
 
     def _read_metadata(self):
         # Read header
@@ -126,15 +216,23 @@ class CoronaArchive:
             f.write(content)
             logging.info("File {} extracted to {}".format(new_filename, self.__output_dir))
 
-    def __repr__(self):
-        return pprint.pformat(vars(self))
-
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING, format='%(asctime)s:%(levelname)-8s %(message)s')
 
-    archive = CoronaArchive()
-    archive.unpack("resource.car", "c:\\Users\\BuRner\\Downloads\\temp1\\")
+    if len(sys.argv) != 4:
+        print "Usage: corona-archiver.py -p 'input_dir' 'output_file'"
+        exit(1)
 
-    print archive.metadata
-    print archive.index
+    method = sys.argv[1]
+    input = sys.argv[2]
+    output = sys.argv[3]
+
+    archiver = CoronaArchiver()
+
+    if method == '-p':
+        archiver.pack(input_dir=input, output_file=output)
+    elif method == '-u':
+        archiver.unpack(input_file=input, output_dir=output)
+    else:
+        print 'Invalid method'
